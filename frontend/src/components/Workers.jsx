b11f1a9bdb13ef222e8048c84d2787027284db87
Adarsh Gaud <adarshgaud5794@gmail.com>
2025-09-20 21:41:59 +0530

    Update image size and styling

--- frontend/src/components/Workers.jsx
+++ frontend/src/components/Workers.jsx
@@ -21,7 +21,7 @@ export default function Workers() {
         <img
           src="https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80"
           alt="Dedicated Workers"
-          className="max-w-5xl w-full object-contain"
+           className="w-[80%] h-[600px] object-cover rounded-lg shadow-lg"
         />
       </div>
     </section>
